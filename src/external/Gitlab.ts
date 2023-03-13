a39b04d3a19e068e2d8b05b6b1b93529cef79be5
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-03-13 15:29:55 -0300

    add initial implemention of gitlab class

--- src/external/Gitlab.ts
+++ src/external/Gitlab.ts
@@ -1,11 +1,47 @@
+import axios, { AxiosInstance } from "axios";
+import GitRemoteHubError from "./GitRemoteHubError";
 import IGitRemoteHub, { UserProfile, UserRepo } from "./IGitRemoteHub";
 
 export default class Gitlab implements IGitRemoteHub {
+  private readonly axiosInstance: AxiosInstance;
+
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: 'https://gitlab.com/api/v4',
+    });
+  }
+
   public async getProfile(criteria: string): Promise<UserProfile | null> {
-    throw new Error("Method not implemented.");
+    try {
+      const response = await this.axiosInstance.get(`/users?username=${criteria}`);
+
+      if (response.data.length === 0) {
+        return null;
+      }
+
+      return {
+        name: response.data[0].name,
+        username: response.data[0].username,
+      };
+    } catch (error: any) {
+      console.log(error.stack);
+
+      throw new GitRemoteHubError();
+    }
   }
 
   public async getProfileRepos(criteria: string): Promise<UserRepo[]> {
-    throw new Error("Method not implemented.");
+    try {
+      const response = await this.axiosInstance.get(`/users/${criteria}/projects`);
+
+      return response.data.map((repo: any) => ({
+        name: repo.name,
+        link: repo.web_url,
+      }));
+    } catch (error: any) {
+      console.log(error.stack);
+
+      throw new GitRemoteHubError();
+    }
   }
 }
