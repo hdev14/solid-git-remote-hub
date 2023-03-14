c785a511c1fbc942198a809b4a8c25f05ccc88fc
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-03-14 10:08:58 -0300

    improvements

--- src/routers/repoRouter.ts
+++ src/routers/repoRouter.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import RepoController from '../controllers/RepoController';
 import Github from '../external/Github';
 import Gitlab from '../external/Gitlab';
+import MongoUserRepository from '../repositories/MongoUserRepository';
+import PrismaUserRepository from '../repositories/PrismaUserRepository';
 import RepoService from '../services/RepoService';
 
 const router = Router();
-const repoController = new RepoController(new RepoService(new Github()));
-// const repoController = new RepoController(new RepoService(new Gitlab()));
+const repoController = new RepoController(new RepoService(new PrismaUserRepository(), new Github()));
+// const repoController = new RepoController(new RepoService(new PrismaUserRepository(), new Gitlab()));
+// const repoController = new RepoController(new RepoService(new MongoUserRepository(), new Github()));
+// const repoController = new RepoController(new RepoService(new MongoUserRepository(), new Gitlab()));
 
 router.get('/:username', repoController.getRepos.bind(repoController));
 
--- src/services/RepoService.ts
+++ src/services/RepoService.ts
@@ -1,12 +1,22 @@
 import Github from "../external/Github";
 import IGitRemoteHub from "../external/IGitRemoteHub";
+import IUserRepository from "../repositories/IUserRepository";
 import IRepoService from "./IRepoService";
 
 export default class RepoService implements IRepoService {
   // Aggregation
-  constructor(private readonly gitRemoteHub: IGitRemoteHub) {}
+  constructor(
+    private readonly userRepository: IUserRepository,
+    private readonly gitRemoteHub: IGitRemoteHub
+  ) {}
 
   public async getUserRepos(username: string) {
+    const user = await this.userRepository.findByUsername(username);
+    
+    if (!user) {
+      return [];
+    }
+
     const repos = await this.gitRemoteHub.getProfileRepos(username);
 
     return repos;
