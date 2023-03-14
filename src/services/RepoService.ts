b82ad54b941d2ab059f4f88a4823e49b4637d165
Hermerson de Araújo <ne.hermerson@gmail.com>
2023-03-14 10:06:49 -0300

    improvements

--- src/services/RepoService.ts
+++ src/services/RepoService.ts
@@ -1,10 +1,20 @@
 import Github from "../external/Github";
+import UserRepository from "../repositories/UserRepository";
 
 export default class RepoService {
   // Aggregation
-  constructor(private readonly github: Github) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly github: Github
+  ) { }
 
   public async getUserRepos(username: string) {
+    const user = await this.userRepository.findByUsername(username);
+    
+    if (!user) {
+      return [];
+    }
+
     const repos = await this.github.getProfileRepos(username);
 
     return repos;
